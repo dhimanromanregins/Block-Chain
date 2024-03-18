64f82663bd69fb16a84b0f6c3e9b23a2d38150ca
sahil <dhimansahil.ameotech@gmail.com>
2024-03-18 17:06:52 +0530

    updates

---
+++ metamask/meta_app/urls.py
@@ -0,0 +1,13 @@
+# urls.py
+from django.urls import path
+from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    path('generate-ethereum-account/', GenerateNetworkAccount.as_view(), name='generate-coin-account'),
+    path('ethereum-balance/<str:address>/<str:symbol>', CoinBalance.as_view(), name='coin-balance'),
+    path('coin-transaction-history/<str:address>/', CoinTransactionHistory.as_view(), name='coin-transaction-history'),
+    path('token_info/<str:address>/', CoinTokenInfo.as_view(), name='token-info'),
+    path('coin_price/', CoinPriceView.as_view(), name='coin_price'),
+    path('send_crypto/', SendCoinView.as_view(), name='send_coin'),
+]
